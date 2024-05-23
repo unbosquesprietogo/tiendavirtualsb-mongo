bfe696e914e9957438f433decbbfafeda03f4a90
David R <jpdramos1000@gmail.com>
2024-05-23 11:26:31 -0500

    /api/

--- src/main/java/com/bo/controller/VentaController.java
+++ src/main/java/com/bo/controller/VentaController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/ventas")
+@RequestMapping("/api/ventas")
 public class VentaController {
 
     @Autowired
